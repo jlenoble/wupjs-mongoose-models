afa9a58fb35918433271b0e9a5861b5f17efa3a6
Jason Lenoble <jason.lenoble@gmail.com>
2017-03-16 18:54:11 +0100

    Use Promises instead of callbacks

--- test/items.test.js
+++ test/items.test.js
@@ -1,89 +1,72 @@
 import {expect} from 'chai';
 import {Item} from '../src';
 
+const getAllItems = length => {
+  return Item.find()
+    .then(items => {
+      expect(items).to.have.length(length);
+      return items;
+    });
+};
+
 describe('Testing Item', function () {
-  it(`Database can be accessed`, function (done) {
-    Item.find({}, (err, items) => {
-      expect(err).to.be.null;
-      expect(items).to.have.length(0);
-      done();
-    });
-  });
+  it(`data can be read from database`, function () {
+    return getAllItems(4)
+      .then(items => {
+        const [item1, item2, item3, item4] = items;
 
-  it(`An item can be created`, function (done) {
-    const item = new Item();
-    item.title = 'Some title';
-
-    item.save(err => {
-      expect(err).to.be.null;
-      done();
-    });
-  });
-
-  it(`An item can be read`, function (done) {
-    const item = new Item();
-    item.title = 'Some other title';
-
-    item.save(err => {
-      expect(err).to.be.null;
-
-      Item.find({_id: item._id}, (err, items) => {
-        expect(err).to.be.null;
-        expect(items).to.have.length(1);
-
-        const {_id, title} = items[0];
-        expect({_id, title}).to.eql({
-          _id: item._id,
-          title: item.title,
-        });
-        done();
+        expect(item1.title).to.equal('Great!');
+        expect(item2.title).to.equal('Super!');
+        expect(item3.title).to.equal('Yes!');
+        expect(item4.title).to.equal('Cool!');
       });
-    });
   });
 
-  it(`An item can be updated`, function (done) {
+  it(`An item can be created`, function () {
     const item = new Item();
-    item.title = 'Another title';
+    item.title = 'Amazing!';
 
-    item.save(err => {
-      expect(err).to.be.null;
+    return item.save()
+      .then(() => getAllItems(5))
+      .then(items => {
+        const [item1, item2, item3, item4, item5] = items;
 
-      Item.update({_id: item._id}, {$set: {
-        title: 'A new title',
-      }}, err => {
-        expect(err).to.be.null;
+        expect(item1.title).to.equal('Great!');
+        expect(item2.title).to.equal('Super!');
+        expect(item3.title).to.equal('Yes!');
+        expect(item4.title).to.equal('Cool!');
+        expect(item5.title).to.equal('Amazing!');
 
-        Item.find({_id: item._id}, (err, items) => {
-          expect(err).to.be.null;
-          expect(items[0].title).to.equal('A new title');
-          done();
-        });
+        expect(item5._id).to.eql(item._id);
       });
-    });
   });
 
-  it(`An item can be deleted`, function (done) {
-    const item = new Item();
-    item.title = 'And another title';
+  it(`An item can be updated`, function () {
+    return getAllItems(5)
+      .then(items => items[4].update({$set: {title: 'Wicked!'}}))
+      .then(() => getAllItems(5))
+      .then(items => {
+        const [item1, item2, item3, item4, item5] = items;
 
-    item.save(err => {
-      expect(err).to.be.null;
-
-      Item.find({}, (err, items) => {
-        expect(err).to.be.null;
-
-        const len = items.length;
-
-        Item.remove({_id: item._id}, err => {
-          expect(err).to.be.null;
-
-          Item.find({}, (err, items) => {
-            expect(err).to.be.null;
-            expect(items.length).to.equal(len - 1);
-            done();
-          });
-        });
+        expect(item1.title).to.equal('Great!');
+        expect(item2.title).to.equal('Super!');
+        expect(item3.title).to.equal('Yes!');
+        expect(item4.title).to.equal('Cool!');
+        expect(item5.title).to.equal('Wicked!');
       });
-    });
+  });
+
+  it(`An item can be deleted`, function () {
+    return getAllItems(5)
+      .then(items => items[0].remove())
+      .then(() => getAllItems(4))
+      .then(items => {
+        const [item2, item3, item4, item5] = items;
+
+        expect(item2.title).to.equal('Super!');
+        expect(item3.title).to.equal('Yes!');
+        expect(item4.title).to.equal('Cool!');
+        expect(item5.title).to.equal('Wicked!');
+      });
   });
 });
